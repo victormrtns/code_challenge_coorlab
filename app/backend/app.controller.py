d1445c804a2a8cfa238b62e885c295ce92e0a8a1
Victor <88071929+victormrtns@users.noreply.github.com>
2024-03-21 00:07:06 -0300

    principal endpoint that is gonna be consumed by the frontend is done

--- app/backend/app.controller.py
+++ app/backend/app.controller.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 # app.config['JSON_AS_ASCII'] = False
 #Here you must to change the directory to works when you clone the repository
 data_path = r"C:\Users\vmhug\OneDrive\Área de Trabalho\code-challenge-coorlab\code_challenge_coorlab\desafio_coorlab\app\data.json"
-with open(data_path, 'r') as myfile:
+with open(data_path,encoding='utf-8') as myfile:
     data=json.load(myfile)
 
 list_of_destinies = []
@@ -25,21 +25,23 @@ def gettravels():
     body = request.args.get('city')
     output_dict = []
     for x in range(0, len(list_of_destinies)):
-        print(list_of_destinies[x]['city'])
-        print(str(body))
         if(list_of_destinies[x]['city'] == str(body)):
             output_dict.append(list_of_destinies[x])
     if(len(output_dict)==1):
         return jsonify(output_dict)
     else:
+        #Initializing Values
         confortprice_duration = int(''.join(c for c in output_dict[0]['duration'] if c.isdigit()))
         economicprice = float(''.join(c for c in output_dict[0]['price_econ'] if (c.isdigit() or c =='.')))
-        economicprice_travel = []
-        confortprice_travel = []
+        economicprice_travel = output_dict[0]
+        confortprice_travel = output_dict[0]
         
         for x in range(0, len(output_dict)):
             formatted_value_economicprice = float(''.join(c for c in output_dict[x]['price_econ'] if (c.isdigit() or c =='.')))
             formatted_value_confortpriceduration = int(''.join(c for c in output_dict[x]['duration'] if c.isdigit()))
+            
+            print(economicprice)
+            print(formatted_value_economicprice)
             
             if(formatted_value_confortpriceduration< confortprice_duration):
                 confortprice_duration = formatted_value_confortpriceduration
@@ -48,7 +50,6 @@ def gettravels():
             if(economicprice> formatted_value_economicprice):
                 economicprice = formatted_value_economicprice
                 economicprice_travel = output_dict[x]
-
     return jsonify([confortprice_travel,economicprice_travel])
 
 app.run(port=5000,host='localhost',debug=True)
--- app/run.sh
+++ app/run.sh
@@ -2,3 +2,4 @@
 
 echo "Implemente aqui o script para executar a sua solução"
 pip install flask
+py -3 -m venv .venv
