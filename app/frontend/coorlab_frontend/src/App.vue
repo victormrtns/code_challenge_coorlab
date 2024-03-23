2d6f7577463f331f97682ef0d13f053abaddb7ef
Victor <88071929+victormrtns@users.noreply.github.com>
2024-03-22 23:35:31 -0300

    Adding frontend vue project with vite and tailwind.css

---
+++ app/frontend/coorlab_frontend/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="text-green-700 text-xl">Hello, from TailwindCSS!</div>
+</template>
+
+<style scoped>
+
+</style>
--- app/run.sh
+++ app/run.sh
@@ -3,3 +3,5 @@
 echo "Implemente aqui o script para executar a sua solução"
 pip install flask
 py -3 -m venv .venv
+npm create vite@latest
+npm install -D tailwindcss
